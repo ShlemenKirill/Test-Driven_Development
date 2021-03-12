79534e8ef41c0d8719004e837b239740c76f687b
Kirill <kirill.shlemen@gmail.com>
2021-03-12 18:22:04 +0300

    configure app for testability

--- LandonHotel/Services/BookingService.cs
+++ LandonHotel/Services/BookingService.cs
@@ -1,11 +1,14 @@
 ﻿using LandonHotel.Data;
+using LandonHotel.Repositories;
 
 namespace LandonHotel.Services
 {
     public class BookingService : IBookingService
     {
-        public BookingService()
+        private readonly IRoomsRepository _roomsRepository;
+        public BookingService(IRoomsRepository roomRepository)
         {
+            _roomsRepository = roomRepository;
         }
 
         public int CalculateBookingCost(int roomId, Booking booking)
